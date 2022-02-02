f44d4879a66cace52385c17d576647ad22edaf0c
Shawn Analla <shawnanalla@gmail.com>
2022-02-01 19:28:35 -0800

    all done except update eomployee and needs polish

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const db = mysql.createConnection({
     database: 'employees_db'
   })
 
+const employees = [];
+const roles = [];
+const departments = [];
+
 const start = () => {  
     inquirer
     .prompt([
@@ -31,18 +35,18 @@ const start = () => {
             case 'View All Employees':
                 viewEmployees();
                 break;
-            // case 'Add a Department':
-            //     addDepartment();
-            //     break;
-            // case 'Add a Role':
-            //     addRole();
-            //     break;
-            // case 'Add an Employee':
-            //     addEmployee();
-            //     break;
-            // case 'Update an Employee Role':
-            //     updateEmployee();
-            //     break;
+            case 'Add a Department':
+                addDepartment();
+                break;
+            case 'Add a Role':
+                addRole();
+                break;
+            case 'Add an Employee':
+                addEmployee();
+                break;
+            case 'Update an Employee Role':
+                updateEmployee();
+                break;
             default:
                 // generate(employees);
                 return;
@@ -76,6 +80,7 @@ const viewRoles = () => {
     // db.end();
     start();
 };
+
 const viewEmployees = () => {
     db.query("SELECT employee.id AS employee_id, employee.first_name, employee.last_name, role.title AS job_title, role.salary, employee.manager_id, department.name AS department_name FROM employee JOIN role ON employee.role_id = role.department_id JOIN department ON role.department_id = department.id", (err,data) => {
         if(err){
@@ -86,6 +91,114 @@ const viewEmployees = () => {
     })
     // db.end();
     start();
+};
+
+
+//Must use SET for db.query for some reason, (names) VALUES doesn't work.
+const addDepartment = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "Please enter the name of the department.",
+            name: 'name'
+        }
+    ]).then((response) => {
+        db.query("INSERT INTO department SET ?", {name: response.name}, (err, data) => {
+            if (err) throw err;
+            console.log(`Department ${response.name} has been added.`)
+            start();
+        })
+
+    })
+
+};
+
+//TODO: On the third input, make a list of departments for the user to select from, and then use the corresponding integer id in the db.query.
+const addRole = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "Please enter the name of the role.",
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: "Please enter the salary for the role (integer, no commas, can contain decimal values).",
+            name: 'salary'
+        },
+        {
+            type: 'input',
+            message: "Please enter the department id for this role (1: Sales, 2: Engineering)",
+            name: 'department_id'
+        }
+    ]).then((response) => {
+        db.query("INSERT INTO role SET ?", 
+        {
+            title: response.title,
+            salary: response.salary,
+            department_id: response.department_id
+        }, (err, data) => {
+            if (err) throw err;
+            console.log(`A role for ${response.title} has been added.`)
+            start();
+        })
+    })
+};
+
+const addEmployee = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "Please enter the first name of the employee.",
+            name: 'first_name'
+        },
+        {
+            type: 'input',
+            message: "Please enter the last name of the employee.",
+            name: 'last_name'
+        },
+        {
+            type: 'input',
+            message: "Please enter this employee's manager id (integer).",
+            name: 'manager_id'
+        },
+        {
+            type: 'input',
+            message: "Please enter the role id for this employee (1: Sales Guy, 2: Software Engineer)",
+            name: 'role_id'
+        }
+    ]).then((response) => {
+        const newEmployee = {
+            first_name: response.first_name,
+            last_name: response.last_name,
+            manager_id: response.manager_id,
+            role_id: response.role_id
+        };
+        employees.push(newEmployee);
+        console.log(employees);
+        db.query("INSERT INTO employee SET ?", newEmployee, (err, data) => {
+            if (err) throw err;
+            console.log(`${response.first_name} ${response.last_name} has been added as an employee.`)
+            start();
+        })
+    })
+};
+
+const updateEmployee = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: "Please select an employee to update.",
+            choices: employees, 
+            name: 'choice'
+        }
+    ]).then((response) => {
+        console.log(response);
+    })
 };
 
 start();
